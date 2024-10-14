cd85f27fd7739aba463363eff3b381bc985dcf52
rguthier <rkg7460@gmail.com>
2024-10-14 11:23:12 -0400

    source code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {ReactComponent as Logo} from "./github.svg"
+import PDF from "./Reese_Guthier_Resume.pdf";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>Reese Guthier</h1>
+        <h3><a href="mailto:rkg460@gmail.com">rguthier25@gmail.com</a></h3>
       </header>
+      <div className="mainContent">
+        <h2>About me</h2>
+        <p>I'm a junior at Towson University majoring in Computer Science with a concentration in Software Engineering. I'm minoring in biology. I have a particular interest in web development. I am interested in software engineering internships in the DMV area. Outside of school, I enjoy horseback riding, reading, and board games.</p>
+        <br />
+        <div className="buttonContainer">
+          <a href={PDF} className="button">Resume</a>
+          <a className="button" href="https://github.com/rguthier"> Github profile <Logo /></a>
+        </div>
+      </div>
     </div>
   );
 }
